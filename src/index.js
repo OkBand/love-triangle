f23009b1ed1fb69c0d2683468f5e9dfacec4abd3
OkBand <o.bandura42@gmail.com>
2018-02-22 22:52:24 +0300

    love triangles

--- src/index.js
+++ src/index.js
@@ -3,5 +3,15 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  var l = preferences.length;
+  var triangles = 0;
+  var x, y, z, i;
+  for (i = 0; i <= l; i++){
+    x = preferences[i];
+    y = preferences[x - 1];
+    z = preferences[y - 1];
+    if (z === i + 1) {
+        triangles++;}
+}
+return Math.floor(triangles/3);
 };
